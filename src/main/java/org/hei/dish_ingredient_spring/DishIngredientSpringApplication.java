3f57787e1fdac2b36e7fabec0c67781a427bf7ec
Miriasme <johndoe@example.com>
2026-03-27 21:04:25 +0300

    Initial commit

---
+++ src/main/java/org/hei/dish_ingredient_spring/DishIngredientSpringApplication.java
@@ -0,0 +1,13 @@
+package org.hei.dish_ingredient_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishIngredientSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishIngredientSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hei/dish_ingredient_spring/DishIngredientSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hei.dish_ingredient_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishIngredientSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
